2315454d87161d8920a92ba3a888cac4d7325bbd
Shimanti <shimanti@gmail.com>
2021-09-10 16:08:27 -0700

    intro index and style

---
+++ links.js
@@ -0,0 +1,7 @@
+links = [
+'https://www.amazon.com/dp/B07X5KSBNJ?tag=shimanti-20',
+'https://www.amazon.com/dp/B01M0GB8CC?tag=shimanti-20',
+'https://www.amazon.com/dp/B07G5FCR6X?tag=shimanti-20',
+'https://www.amazon.com/dp/B004K95PBQ?tag=shimanti-20',
+'https://www.amazon.com/dp/B08N5LNQCX?tag=shimanti-20'
+]
